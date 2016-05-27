b939db1f5411fd80f608a3ee0d4f6f5500b5239b
soulharu <sbramosj@gmail.com>
2016-05-27 21:42:30 +0000

    Start

---
+++ lonestar/main.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE QuasiQuotes           #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
+import           Yesod
+
+data HelloWorld = HelloWorld
+
+mkYesod "HelloWorld" [parseRoutes|
+/ HomeR GET
+|]
+
+instance Yesod HelloWorld
+
+getHomeR :: Handler Html
+getHomeR = defaultLayout [whamlet|
+    <h1>Hello World!
+|]
+
+main :: IO ()
+main = warp 8080 HelloWorld
