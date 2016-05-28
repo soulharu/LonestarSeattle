94a6b992c43ca7bb244227613b069726e927e690
soulharu <sbramosj@gmail.com>
2016-05-28 21:56:48 +0000

    Cabal atualizado, adicionado arquivos hamlet,lucius e julius para a rota Home

--- lonestar/main.hs
+++ lonestar/main.hs
@@ -2,7 +2,12 @@
 {-# LANGUAGE QuasiQuotes           #-}
 {-# LANGUAGE TemplateHaskell       #-}
 {-# LANGUAGE TypeFamilies          #-}
-import           Yesod
+import Yesod
+import Database.Persist.Postgresql
+import Data.Text
+import Text.Lucius
+import Text.Julius
+import Control.Monad.Logger (runStdoutLoggingT)
 
 data HelloWorld = HelloWorld
 
@@ -13,9 +18,13 @@ mkYesod "HelloWorld" [parseRoutes|
 instance Yesod HelloWorld
 
 getHomeR :: Handler Html
-getHomeR = defaultLayout [whamlet|
-    <h1>Hello World!
-|]
+getHomeR = defaultLayout $ do
+           toWidget $ $(luciusFile "templates/home.lucius")
+           toWidgetHead [hamlet|
+             <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js">
+           |]
+           $(whamletFile "templates/home.hamlet")
+           toWidgetHead $ $(juliusFile "templates/home.julius")
 
 main :: IO ()
 main = warp 8080 HelloWorld
