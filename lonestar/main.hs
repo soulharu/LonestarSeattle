796bd1d143d1832bc41f2f0c904c582a52224d40
soulharu <sbramosj@gmail.com>
2016-06-02 19:49:18 +0000

    Adicionada conexão com o banco e Alterações feitas na Home

--- lonestar/main.hs
+++ lonestar/main.hs
@@ -1,7 +1,7 @@
-{-# LANGUAGE OverloadedStrings     #-}
-{-# LANGUAGE QuasiQuotes           #-}
-{-# LANGUAGE TemplateHaskell       #-}
-{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE OverloadedStrings, TypeFamilies, QuasiQuotes,
+             TemplateHaskell, GADTs, FlexibleInstances,
+             MultiParamTypeClasses, DeriveDataTypeable,
+             GeneralizedNewtypeDeriving, ViewPatterns, EmptyDataDecls #-}
 import Yesod
 import Database.Persist.Postgresql
 import Data.Text
@@ -9,7 +9,23 @@ import Text.Lucius
 import Text.Julius
 import Control.Monad.Logger (runStdoutLoggingT)
 
-data HelloWorld = HelloWorld
+data HelloWorld = HelloWorld{connPool :: ConnectionPool}
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Policiais json
+   login Text
+   senha Text
+   nome Text
+   idade Int
+   raca Text
+   sinn Int
+   sinntype Text
+   matrixid Int
+   patente Text
+   
+   deriving Show
+|]
+
 
 mkYesod "HelloWorld" [parseRoutes|
 / HomeR GET
@@ -17,6 +33,8 @@ mkYesod "HelloWorld" [parseRoutes|
 |]
 
 instance Yesod HelloWorld
+
+type Form a = Html -> MForm Handler (FormResult a, Widget)
 
 getHomeR :: Handler Html
 getHomeR = defaultLayout $ do
@@ -35,6 +53,11 @@ getErroR = defaultLayout $ do
              <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Orbitron">
            |]
            $(whamletFile "templates/erro.hamlet")
+           
 
-main :: IO ()
-main = warp 8080 HelloWorld
+connStr = "dbname=d3asuujt2vg6o1 host=ec2-54-163-226-48.compute-1.amazonaws.com user=isonzxoxadmqir password=wpDkE8ysUDGhWNfHoBZoCzx5CT port=5432"
+
+main::IO()
+main = runStdoutLoggingT $ withPostgresqlPool connStr 10 $ \pool -> liftIO $ do 
+       runSqlPersistMPool (runMigration migrateAll) pool
+       warp 8080 (HelloWorld pool)
