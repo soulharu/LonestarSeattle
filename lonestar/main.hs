1536c90618b508e2758f8c5b3bf832deb4cb2601
soulharu <sbramosj@gmail.com>
2016-06-02 19:57:01 +0000

    Adicionado metodo GET da rota de Cadastro

--- lonestar/main.hs
+++ lonestar/main.hs
@@ -30,11 +30,27 @@ Policiais json
 mkYesod "HelloWorld" [parseRoutes|
 / HomeR GET
 /denied ErroR GET
+/singup CadastroR GET
 |]
 
 instance Yesod HelloWorld
 
 type Form a = Html -> MForm Handler (FormResult a, Widget)
+
+instance RenderMessage HelloWorld FormMessage where
+    renderMessage _ _ = defaultFormMessage
+    
+formPoliciais :: Form Policiais
+formPoliciais = renderDivs $ Policiais <$>
+           areq textField "Login: " Nothing <*>
+           areq passwordField "Senha: " Nothing <*>
+           areq textField "Nome: " Nothing <*>
+           areq intField "Idade: " Nothing <*>
+           areq textField "Raça: " Nothing <*>
+           areq intField "SIN: " Nothing <*>
+           areq textField "SIN Type: " Nothing <*>
+           areq intField "Matix ID: " Nothing <*>
+           areq textField "Patente: " Nothing
 
 getHomeR :: Handler Html
 getHomeR = defaultLayout $ do
@@ -53,6 +69,15 @@ getErroR = defaultLayout $ do
              <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Orbitron">
            |]
            $(whamletFile "templates/erro.hamlet")
+           
+getCadastroR :: Handler Html
+getCadastroR = do
+           (widget, enctype) <- generateFormPost formPoliciais
+           defaultLayout [whamlet|
+                 <form method=post enctype=#{enctype} action=@{CadastroR}>
+                     ^{widget}
+                     <input type="submit" value="Enviar">
+           |]
            
 
 connStr = "dbname=d3asuujt2vg6o1 host=ec2-54-163-226-48.compute-1.amazonaws.com user=isonzxoxadmqir password=wpDkE8ysUDGhWNfHoBZoCzx5CT port=5432"
